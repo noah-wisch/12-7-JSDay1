dfb4f10f2fd4bf31c9a33be612a947a3a7bc1d5d
Noah Wisch <noah.wisch@gmail.com>
2016-12-08 16:08:26 -0500

    Updated HW7 to reflect class review

--- HW7/homework.js
+++ HW7/homework.js
@@ -15,47 +15,83 @@ console.log(tallest);
 
 /* ------------------------------------------ */
 
-// console.log('#2: Hangman Lite'); 
+console.log('#2: Hangman Lite'); 
 
-// let newword = 'Clemson'
-// let letter = 'm'
-// let hangman = true //if the guess is right
+let newWord = 'Salad'
+let letter = 'S'
+let occurances = 0; //number used for counting the # of appearances 
 
-// let (i=0; i < newword.length; i++) {
-//     if (letter[0] !== newword.length[i]) {
-//         hangman = false;
-//     }
-// }
 /*
-
-Take the guess and match it against my word
-If the letter guessed does not exist in my word, 
-UGH idk man...
-
+1. Iterate over the word to see if the letter is in the word
+2. If the current letter is 'a' add one to the occurances 
+3. Print out 'it exists x times' if occurances > 0 
+4. If occurances === 0 print 'not here'
 */
+
+for (let i = 0; i < newWord.length; i++) {
+    if (newWord[i] === letter) { //this is step 2 from above
+        occurances++;
+    }
+}
+
+if (occurances > 0) {
+    console.log('it exists ' + occurances + ' times');
+} else {
+    console.log('Nope, not here');
+}
 
 /* ------------------------------------------ */
 console.log('#3: Cherokee Hare');
 
-let startingPopulation = 0;
+let startingPopulation = 200;
 let birthRate = 0.1;
 let numberOfWeeks = 5;
-let population = true //idk what to run to make this work. I feel like my formula isn't SUPER far off, but yeah...
+// let population = true //idk what to run to make this work. I feel like my formula isn't SUPER far off, but yeah...
 
-for (let index = 0; index < startingPopulation; index++) {
-    if (birthRate[index] === numberOfWeeks[index]) {
+// for (let index = 0; index < startingPopulation; index++) {
+//     if (birthRate[index] === numberOfWeeks[index]) {
 
-    }
+//     }
+// }
+// console.log(population);
+
+
+// After one week, 200 x 1.1
+// 1. Add solution from above to the startingPopulation
+// 2. Repeat numberOfWeeks times
+
+let finalPop = startingPopulation;
+for (let i = 0; i < numberOfWeeks; i++) {
+    finalPop = finalPop + (finalPop * birthRate);
 }
-console.log(population);
 
+finalPop = Math.floor(finalPop);
 
-// There will be x Cherokee Hares after y weeks" but replace x with the number of hares and y with the number of weeks.
+console.log('Rabbit horde size' + finalPop);
 
 /* ------------------------------------------ */
 console.log('#4: Change Machine');
 
-let amount = [20,10,5,1]
+let amount = 66;
+
+// [20,10,5,1]
+
+// Number of complete 20s we can pull out
+let twenties = Math.floor(amount / 20);
+// Bills left to provide change for 
+amount = amount % 20;
+
+let tens = Math.floor(amount / 10);
+amount = amount % 10;
+
+let fives = Math.floor(amount / 5);
+amount = amount % 5;
+
+let ones = amount;
+
+let change = [twenties, tens, fives, ones];
+console.log(change)
+
 
 /* Okay, for starters, I know I need to use Modulus...
 I think I need to assign values of 0-3 for 20s, 10s, 5s and 1s. 
